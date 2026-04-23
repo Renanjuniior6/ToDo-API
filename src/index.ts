a1c51e8c47b9bf3bdcb5823c109215dd774b315a
Renan <renanjrr679@gmail.com>
2026-04-23 16:12:17 -0300

    feat: conexao com o mysql estabelecida

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { getConnection } from './database/connection';
 
 const app = express();
 
@@ -8,6 +9,13 @@ app.get('/health', (req, res) => {
     return res.json({ status: 'ok'});
 })
 
-app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000');
-});
+async function startServer() {
+    await getConnection();
+
+    app.listen(3000, () => {
+        console.log('Servidor rodando na porta 3000');
+    });
+}
+
+startServer();
+
