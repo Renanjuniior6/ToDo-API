4f8588234975a6262818a1035ee5e452e9c8111a
Renan Junior <renan679@gmail.com>
2026-04-30 22:05:55 -0300

    refactor: tratamento de erros com zod no middleware

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,6 +1,14 @@
 import { NextFunction, Request, Response } from "express";
+import { ZodError } from "zod";
 
 export function errorMiddleware(error: any, req: Request, res: Response, next: NextFunction) {
+
+    if(error instanceof ZodError) {
+        res.status(400).json({
+            error: "Dados inválidos",
+            details: error.issues
+        });
+    }
     console.error(error);
 
     return res.status(400).json({
