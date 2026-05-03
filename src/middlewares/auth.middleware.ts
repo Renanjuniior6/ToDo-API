b816bd44b79c7b5e493c8d79818b103012a70694
Renan Junior <renan679@gmail.com>
2026-05-03 13:16:16 -0300

    feature: controller e middleware de autenticacao criados

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from 'jsonwebtoken';
+
+const JWT_SECRET = process.env.JWT_SECRET || "secret";
+
+export function authMiddleware (req: Request, res: Response, next: NextFunction) {
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader) {
+        return res.status(401).json({ error: "Token não informado"});
+    }
+
+    const [, token] = authHeader.split("");
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET) as any
+
+        (req as any).userId = decoded.userId;
+        
+        return next();
+    } catch {
+        return res.status(401).json({ error: "Token inválido" });
+    }
+
+}
